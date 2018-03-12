9b1ba9abab90eba6a5fcb60958b36c9a978bd754
Garrett Heath Koller <garrettheath4@gmail.com>
2018-03-12 15:30:57 -0400

    Removed haystack-fullscreen.sh script in favor of using Tampermonkey user script to make video element take up the whole page on Haystack.tv

--- autorefresh-chromium.sh
+++ autorefresh-chromium.sh
@@ -2,18 +2,11 @@
 # This script is called from ~/.config/lxsession/LXDE-pi/autostart
 # Source: https://www.raspberrypi.org/forums/viewtopic.php?t=178206
 
-fullscreen_script=~/rpi-chrome-display/haystack-fullscreen.sh
-
 export DISPLAY=:0.0
 
 while true; do
 	# sleep arg is in seconds, so 12 hours = 43200 seconds
 	sleep 43200
 	xdotool key 'F5'
-	if [ -x $fullscreen_script ]; then
-		$fullscreen_script
-	else
-		echo "$(date) - $fullscreen_script script either not found or is not executable" >> ~/autorefresh-chromium.log
-	fi
 done
 
