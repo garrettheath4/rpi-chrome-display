6069d24c44ed56ffd0e5f24063563076906c8b09
Garrett Heath Koller <garrettheath4@gmail.com>
2019-11-06 16:46:52 -0500

    Chromium now navagates to whatever URL is in a url.txt file if it exists or google.com if it does not exist

--- autostart.sh
+++ autostart.sh
@@ -7,4 +7,5 @@ xset -dpms
 # Hide the banner to restore the previous Chromium browser session if there was an unexpected shutdown
 sed --in-place=bak -e 's/"exited_cleanly": \?false/"exited_cleanly":true/' -e 's/"exit_type": \?Crashed/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 # Start Chromium in kiosk mode
-chromium-browser --noerrdialogs --kiosk http://www.google.com/ --incognito
+url_to_open=$(cat "$HOME/rpi-chrome-display/url.txt")
+chromium-browser --noerrdialogs --kiosk ${url_to_open:-http://www.google.com/} --incognito
