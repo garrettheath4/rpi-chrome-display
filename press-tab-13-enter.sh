a9c3e415ea7cc133192bdd96c6bdb81cb21373d7
Garrett Heath Koller <garrettheath4@gmail.com>
2019-12-05 17:13:30 -0500

    Added line to press-tab-13-enter.sh to switch current directory to the directory containing the script, in case it is called from a different directory since it needs to source utils.sh from the current directory

--- press-tab-13-enter.sh
+++ press-tab-13-enter.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Switch to this script's directory (in case it's called from a different working directory)
+cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
+
 # shellcheck disable=SC1091
 source utils.sh
 
